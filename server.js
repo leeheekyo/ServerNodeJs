5b27925dfaa7cffc5023bbf34fe575e161acc0c9
leeheekyo <lhk5895@gmail.com>
2017-06-01 21:05:01 +0900

    update page

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var PORT = process.env.PORT || 3000;
 
 var express = require('express');
 var fs = require("fs");
+var bodyParser = require('body-parser');
+var querystring = require('querystring');
 var app = express();
+
+var dateFormat = require('dateformat');
 
 //db value
 var MongoClient = require('mongodb').MongoClient;
@@ -23,6 +27,7 @@ var db = new Db('local', new Server('localhost', 27017));
 db.open(function(err, db) {
   if(err) console.log(err);
   else console.log("db open");
+
 })
 
 app.get('/', function(req, res){
@@ -47,6 +52,7 @@ app.get('/board.html', function(req, res){
     fs.readFile('board_before.html', function(error,data_before){
         res.writeHead(200, {'Content-Type':'text/html'})
         
+        //find DB and show
         var collection = db.collection("board");
         var data = data_before;
         collection.find().toArray(function(err, items) {
@@ -75,10 +81,17 @@ app.get('/board.html', function(req, res){
         
     });
 });
+app.get('/QnA_add.html', function(req, res){
+    fs.readFile('QnA_add.html', function(error,data){
+        res.writeHead(200, {'Content-Type':'text/html'})
+        res.end(data);
+    });
+});
 app.get('/QnA.html', function(req, res){
     fs.readFile('QnA_before.html', function(error,data_before){
         res.writeHead(200, {'Content-Type':'text/html'})
         
+        //find DB and show.
         var collection = db.collection("QnA");
         var data = data_before;
         collection.find().toArray(function(err, items) {
@@ -123,7 +136,121 @@ app.get('/QnA.html', function(req, res){
     });
 });
 app.post('/loginok', function(req, res) {
-    console.log("post received: %s %s", req.query.id, req.query);
+    /*if(req.method == 'GET') { 
+        fs.readFile('./docRoot/form.html', function(err, data) { 
+            res.writeHead(200, {'Content-Type':'text/html'}); 
+            res.end(data); 
+        }); 
+    }*/
+    //post parameter passing
+    if(req.method =='POST') { 
+        req.on('data', function(chunk) {
+            console.log(chunk.toString()); 
+            var data = querystring.parse(chunk.toString()); 
+            res.writeHead(200, {'Content-Type':'text/html'}); 
+            
+            /*
+            var myobj = { name: "Company Inc", address: "Highway 37" };
+                db.collection("customers").insertOne(myobj, function(err, res) {
+                if (err) throw err;
+                console.log("1 record inserted");
+                db.close();
+            });*/
+            
+            //check id and password
+        var query = { id: data.id, pw : data.pw };
+        db.collection("user").find(query).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            
+            if(result=="") res.end("login fail");
+            else {
+                res.end("login success <script>self.location='/';</script>");
+                
+            }
+        });
+            
+            
+            
+        }); 
+    }
+
+});
+app.post('/board_add_ok', function(req, res) {
+    
+if(req.method =='POST') { 
+    req.on('data', function(chunk) { 
+        console.log(chunk.toString());
+        
+        
+        
+        var data = querystring.parse(chunk.toString()); 
+        res.writeHead(200, {'Content-Type':'text/html'}); 
+        
+        //calculate id
+        var query = {  };
+        var boardCount = 0;
+        db.collection("board").find(query).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            boardCount=result[result.length-1].id;
+            console.log(boardCount);
+        });
+        boardCount+=1;
+        
+        //calculate date
+        var date=dateFormat(result.request_date, "yyyy-mm-dd");
+        console.log('[' + date + '] ' + '현재 시간')
+
+        //insert board
+        var myobj = { id:boardCount, title: data.title, body: data.body, day : date };
+        db.collection("board").insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            console.log("1 record inserted");
+            //db.close();
+        });
+        
+        res.end('title : ' + data.title + ',body : ' + data.body); 
+    });
+}
+});
+app.post('/QnA_add_ok', function(req, res) {
+
+if(req.method =='POST') { 
+    req.on('data', function(chunk) { 
+        console.log(chunk.toString());
+        
+        
+        
+        var data = querystring.parse(chunk.toString()); 
+        res.writeHead(200, {'Content-Type':'text/html'}); 
+        
+        //calculate id
+        var query = {  };
+        var boardCount = 0;
+        db.collection("QnA").find(query).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            boardCount=result[result.length-1].id;
+            console.log(boardCount);
+        });
+        boardCount+=1;
+        
+        //calculate date
+        var date=dateFormat(result.request_date, "yyyy-mm-dd");
+        console.log('[' + date + '] ' + '현재 시간')
+
+        //insert QnA
+        var myobj = { id:boardCount, title: data.title, body: data.body, day : date };
+        db.collection("QnA").insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            console.log("1 record inserted");
+            //db.close();
+        });
+        
+        res.end('title : ' + data.title + ',body : ' + data.body); 
+    });
+}
 });
 
 app.listen(PORT, function(){
